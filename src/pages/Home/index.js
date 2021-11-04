1ca987d151bd31b8b23c236c495cea0b4d89ca34
Keval Solanki <keval8solanki@gmail.com>
2021-11-04 13:43:17 +0530

    added random names

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,11 +9,25 @@ import {
 	OptionContainer,
 	Title,
 } from './homepage.styled'
-import { v4 } from 'uuid'
+
+import {
+	uniqueNamesGenerator,
+	adjectives,
+	colors,
+	animals,
+} from 'unique-names-generator'
+
+const customConfig = {
+	dictionaries: [colors, adjectives, animals],
+	separator: '-',
+	length: 3,
+}
 
 export default function HomePage() {
 	const [newRoom, setNewRoom] = useState(false)
-	const [roomname, setRoomname] = useState(() => v4())
+	const [roomname, setRoomname] = useState(() =>
+		uniqueNamesGenerator(customConfig)
+	)
 
 	return (
 		<Container>
